5af6ec1dcf53106ddf524ba23c482773fb09df0a
AnayaThombare <anayathombare@gmail.com>
2025-12-04 18:47:49 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
-print("Hello World") 
+class Library:
+  def __init__(self,zip_code):
+    self.zip_code = zip_code
+    
+  def display_zip(self):
+    print(self.zip_code)
+
+ala_lib = Library(439875)
+
+ala_lib.display_zip()
+
